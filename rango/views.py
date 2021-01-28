072f447d3c1f035d2c42d8872aa3e9cb9c98c99a
Soyaba Hasan <2379221h@student.gla.ac.uk>
2021-01-28 19:05:28 +0000

    Chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -8,13 +8,12 @@ from rango.models import Category, Page
 def index(request):
     # return HttpResponse("Rango says hey there partner! <a href='/rango/about'>Index</a>")
     category_list = Category.objects.order_by('-likes')[:5]
-
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
-    
-  
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
     # context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -28,6 +27,7 @@ def show_category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
+        
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
